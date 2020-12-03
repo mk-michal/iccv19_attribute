1cd521df7f0204cb1e865ce03ab9f9baf6cc16b8
Michal Kucirka <michal.kucirka@cogniware.com>
2020-12-03 10:10:06 +0100

    deleted hard-coded weights from loss function. Weights could be added as an argument do loss class

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Build setup
 setup(
     name='iccv19_attribute',
-    version='1.0.0',
+    version='1.1.0',
     license='',
     author='ang, Chufeng and Sheng, Lu and Zhang, Zhaoxiang and Hu, Xiaolin',
     url='https://github.com/chufengt/iccv19_attribute',
     packages=find_packages(),
     description='Improving Pedestrian Attribute Recognition With Weakly-Supervised Multi-Scale '
                 'Attribute-Specific Localization',
-    install_requires=['torch==1.3.1', 'numpy==1.19.4'],
     include_package_data=True,
     data_files=[('.', ['README.md', 'requirements.txt'])],
 )
